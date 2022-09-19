d16b67eee7cb42d5cca018dfc20f68b62b2e4247
abhi1518 <60543134+abhi1518@users.noreply.github.com>
2022-09-19 13:13:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import Navb from './component/Navb'
+
+function App() {
+  return (
+    <div>
+      <Navb />
+     <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
